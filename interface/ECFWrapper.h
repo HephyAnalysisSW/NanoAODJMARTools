8b3cd366bcd0c1b53ea2c6b8ee8e50a5da18ccf3
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-09-16 17:24:54 +0200

    restructure

--- interface/ECFWrapper.h
+++ interface/ECFWrapper.h
@@ -5,26 +5,31 @@
 #include "fastjet/ClusterSequence.hh"
 #include "fastjet/JetDefinition.hh"
 #include "TLorentzVector.h"
+#include "TVectorT.h"
 
 #include "fastjet/contrib/EnergyCorrelator.hh"
+#include "fastjet/PseudoJet.hh"
 
 #include <string>
 #include <vector>
 
 class ECFWrapper {
-    
-    public:
-        ECFWrapper( unsigned N, double alpha, double beta, std::string ecftype, unsigned  measure=0 );
-        ECFWrapper();
-        double result( std::vector<fastjet::PseudoJet> );
-    private:
-        unsigned N_;
-        double   alpha_;
-        double   beta_;
-        std::string ecftype_;
-        unsigned measure_;
 
-        std::shared_ptr<fastjet::FunctionOfPseudoJet<double>> routine_;
+    public:
+        ECFWrapper();
+
+        void addECF( unsigned N, double alpha, double beta, std::string ecftype, unsigned  measure=0 );
+
+        void setParticles( const std::vector<TLorentzVector> & particles );
+        std::vector<double> result( );
+
+    private:
+        fastjet::contrib::EnergyCorrelator::Measure pt_R_;
+        fastjet::contrib::EnergyCorrelator::Measure E_theta_;
+        fastjet::contrib::EnergyCorrelator::Measure E_inv_;
+
+        std::vector<std::shared_ptr<fastjet::FunctionOfPseudoJet<double>>> routines_;
+        std::vector<fastjet::PseudoJet> particles_;
 };
 #endif
 
--- src/ECFWrapper.cc
+++ src/ECFWrapper.cc
@@ -1,39 +1,56 @@
 #include "PhysicsTools/NanoAODJMARTools/interface/ECFWrapper.h"
 #include "fastjet/PseudoJet.hh"
+#include "TLorentzVector.h"
+#include <vector>
 
-ECFWrapper::ECFWrapper( unsigned N, double alpha, double beta, std::string ecftype, unsigned  measure):
-    N_(N), alpha_(alpha), beta_(beta), ecftype_(ecftype), measure_(measure)
+ECFWrapper::ECFWrapper( )
 {
-    fastjet::contrib::EnergyCorrelator::Measure pt_R_( fastjet::contrib::EnergyCorrelator::pt_R );
-    fastjet::contrib::EnergyCorrelator::Measure E_theta_( fastjet::contrib::EnergyCorrelator::E_theta );
-    fastjet::contrib::EnergyCorrelator::Measure E_inv_( fastjet::contrib::EnergyCorrelator::E_inv );
+    pt_R_    =  fastjet::contrib::EnergyCorrelator::pt_R;
+    E_theta_ =  fastjet::contrib::EnergyCorrelator::E_theta;
+    E_inv_   = fastjet::contrib::EnergyCorrelator::E_inv;
+}
 
+void ECFWrapper::addECF( unsigned N, double alpha, double beta, std::string ecftype, unsigned  measure)
+{
     fastjet::contrib::EnergyCorrelator::Measure const * measureDef = nullptr;
-    switch ( measure_ ) {
+    switch ( measure ) {
         case fastjet::contrib::EnergyCorrelator::pt_R:    measureDef = &pt_R_; break;
         case fastjet::contrib::EnergyCorrelator::E_theta: measureDef = &E_theta_; break;
         case fastjet::contrib::EnergyCorrelator::E_inv:   measureDef = &E_inv_; break;
     };
 
-    if (ecftype_ == "ECF" || ecftype_.empty()) {
-      routine_.reset( new fastjet::contrib::EnergyCorrelator(N_, beta_, *measureDef));
-    } else if (ecftype_ == "C") {
-      routine_.reset( new fastjet::contrib::EnergyCorrelatorCseries(N_, beta_, *measureDef));
-    } else if (ecftype_ == "D") {
-      routine_.reset( new fastjet::contrib::EnergyCorrelatorGeneralizedD2(alpha_, beta_, *measureDef));
-    } else if (ecftype_ == "N") {
-      routine_.reset(new fastjet::contrib::EnergyCorrelatorNseries(N_, beta_, *measureDef));
-    } else if (ecftype_ == "M") {
-      routine_.reset(new fastjet::contrib::EnergyCorrelatorMseries(N_, beta_, *measureDef));
-    } else if (ecftype_ == "U") {
-      routine_.reset(new fastjet::contrib::EnergyCorrelatorUseries(N_, beta_, *measureDef));
+    std::shared_ptr<fastjet::FunctionOfPseudoJet<double>> routine_;
+
+    if (ecftype == "ECF" || ecftype.empty()) {
+      routine_.reset( new fastjet::contrib::EnergyCorrelator(N, beta, *measureDef));
+    } else if (ecftype == "C") {
+      routine_.reset( new fastjet::contrib::EnergyCorrelatorCseries(N, beta, *measureDef));
+    } else if (ecftype == "D") {
+      routine_.reset( new fastjet::contrib::EnergyCorrelatorGeneralizedD2(alpha, beta, *measureDef));
+    } else if (ecftype == "N") {
+      routine_.reset(new fastjet::contrib::EnergyCorrelatorNseries(N, beta, *measureDef));
+    } else if (ecftype == "M") {
+      routine_.reset(new fastjet::contrib::EnergyCorrelatorMseries(N, beta, *measureDef));
+    } else if (ecftype == "U") {
+      routine_.reset(new fastjet::contrib::EnergyCorrelatorUseries(N, beta, *measureDef));
+    }
+
+    routines_.push_back( routine_ );
+}
+
+void ECFWrapper::setParticles( const std::vector<TLorentzVector> & particles ) {
+
+    particles_.clear() ;
+    for (std::vector<TLorentzVector>::const_iterator p = particles.begin(); p!=particles.end(); ++p ) {
+        particles_.push_back(fastjet::PseudoJet(p->Px(), p->Py(), p->Pz(), p->Energy()));
     }
 }
 
-ECFWrapper::ECFWrapper() : 
-    N_(1), alpha_(1.), beta_(1.), ecftype_("ECF"), measure_( fastjet::contrib::EnergyCorrelator::pt_R ) 
-{};
-
-double ECFWrapper::result( std::vector<fastjet::PseudoJet> particles ) {
-    return routine_ ->result( join(particles) );
+std::vector< double > ECFWrapper::result( ) {
+    std::vector< double > results;
+    results.clear();
+    for ( std::vector<std::shared_ptr<fastjet::FunctionOfPseudoJet<double>>>::iterator routine_ = routines_.begin(); routine_!=routines_.end(); ++routine_ ) {
+        results.push_back( (*routine_)->result( join(particles_) ) );
+    }
+    return results; 
 }
